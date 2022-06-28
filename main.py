5b9c7e2e313707f041445a0beaf3f4ea2c9db46f
Disunic <83856385+Disunic20@users.noreply.github.com>
2022-06-28 15:50:43 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,54 +1,112 @@
-import http.server
-from multiprocessing.connection import wait
-from colored import fg
-import socketserver
 import random
 import sys
-back = fg("#2196f3")
-print(back+"""
+from multiprocessing.connection import wait
+import os
+from DIOS.DIOS_Colored import cl
+from DIOS.res import socketserver
+from DIOS.res import webbrowser
+from DIOS.res.http import server as sv
 
+backCl  =     cl("#2196f3")
+helpcl  =     cl("#31ff79") 
+inputCl =     cl("#20b2aa")
+urlCl   =     cl("#ff9800")
+pathCl  =     cl("#3f51b5")
+pyvCl   =     cl("#e91e63")
 
-░█▀▀▄ ▀█▀ ░█▀▀▀█ ░█▀▀▀█   ░█─── █▀▀█ █▀▀ █▀▀█ █──   ░█─░█ █▀▀█ █▀▀ ▀▀█▀▀ 
-░█─░█ ░█─ ░█──░█ ─▀▀▀▄▄   ░█─── █──█ █── █▄▄█ █──   ░█▀▀█ █──█ ▀▀█ ──█── 
-░█▄▄▀ ▄█▄ ░█▄▄▄█ ░█▄▄▄█   ░█▄▄█ ▀▀▀▀ ▀▀▀ ▀──▀ ▀▀▀   ░█─░█ ▀▀▀▀ ▀▀▀ ──▀──
-                                                                                __________________________________
-                                                                               |                                  |
-                                                                               |     From Disunic Corporation     |
-                                                                               |__________________________________|
-                                                                               """)
-inp = fg("#20b2aa")
-ul = fg("#ff9800")
-pa = fg("#3f51b5")
-py = fg("#e91e63")
-def et():
-    Disunic = input(">>>")
+def logo():
+    print(backCl+"""
+
+    ░█▀▀▄ ▀█▀ ░█▀▀▀█ ░█▀▀▀█   ░█─── █▀▀█ █▀▀ █▀▀█ █──   ░█─░█ █▀▀█ █▀▀ ▀▀█▀▀ 
+    ░█─░█ ░█─ ░█──░█ ─▀▀▀▄▄   ░█─── █──█ █── █▄▄█ █──   ░█▀▀█ █──█ ▀▀█ ──█── 
+    ░█▄▄▀ ▄█▄ ░█▄▄▄█ ░█▄▄▄█   ░█▄▄█ ▀▀▀▀ ▀▀▀ ▀──▀ ▀▀▀   ░█─░█ ▀▀▀▀ ▀▀▀ ──▀──
+                     __________________________________
+                    |                                  |
+                    |     From Disunic Corporation     |
+                    |__________________________________|
+                                                                                """)
+logo()
+def helping():print(helpcl+"""
+        # Help Section :
+
+        Type : " 1 " , to start local host in random server.
+
+        Type : " 2 " , to start local host,in the port number which typed by you.
+    """)
+helping()
+def start():
+    print("")
+    Disunic = input(" DIOS: ")
+    print("")
     if (Disunic == "exit"):
         exit()
+    elif (Disunic == "help"):
+        helping()
+        return(start())
+    elif (Disunic == "1"):
+        rand()
+    elif (Disunic == "2"):
+        mannual()
+    elif (Disunic == "clear"):
+        os.system('cls')
+        logo()
+        rand(start())
     else:
-        print("Error")
-        return(et())
-def main():
-    DIRECTORY = input(inp+" Path :  ")
-    PORT = random.randint(1, 10000)
+        print(" Error command found 🤔!")
+        return(start())
+def rand():
+    # global PORT1
+    print("")
+    DIRECTORY = input(inputCl+" Path :  ")
+    PORT1 = random.randint(1, 10000)
     S1 = "http://127.0.0.1"
-    OJ = S1+':'+str(PORT)
-    class Handler(http.server.SimpleHTTPRequestHandler):
+    OJ = S1+':'+str(PORT1)
+    class Handler(sv.SimpleHTTPRequestHandler):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, directory=DIRECTORY, **kwargs)
+    with socketserver.TCPServer(("", PORT1), Handler) as httpd:
+                print("""                                       """)
+                print(urlCl+" Url  :", OJ                          )
+                print("                                           ")
+                print(pathCl+" Path : ", DIRECTORY                 )
+                print("                                           ")
+                print(pathCl+" Server Hosted Succesfully.         ")
+                print("                                           ")
+                webbrowser.open(OJ                                 )
+                print("Browser Loaded Succesfully. "               )
+                print(pyvCl+"| Python Version - ", sys.version, "|")
+                print("                                           ")
+                print("                                           ")
+                httpd.serve_forever()
+
+def mannual():
+    print("")
+    # print(PORT1)
+    DIRECTORY = input(inputCl+" Path :  ")
+    PORT = int(input(" PORT : "))
+    if (PORT>60000):
+        print("")
+        print(" Port Must Be 0 to 60000 ")
+        return(mannual())
+    else:pass
+    S1 = "http://127.0.0.1"
+    OJ = S1+':'+ str(PORT) 
+    class Handler(sv.SimpleHTTPRequestHandler):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, directory=DIRECTORY, **kwargs)
     with socketserver.TCPServer(("", PORT), Handler) as httpd:
                 print("""                                       """)
-                print(ul+" Url  :", OJ                             )
+                print(urlCl+" Url  :", OJ                          )
                 print("                                           ")
-                print(pa+" Path : ", DIRECTORY                     )
+                print(pathCl+" Path : ", DIRECTORY                 )
                 print("                                           ")
-                print(pa+" Server Hosted Succesfully.")
+                print(pathCl+" Server Hosted Succesfully.         ")
+                webbrowser.open(OJ                                 )
                 print("                                           ")
-                print(py+"| Python Version - ", sys.version, "|   ")
+                print("Browser Loaded Succesfully. "               )
+                print(pyvCl+"| Python Version - ", sys.version, "|")
                 print("                                           ")
-                # print("Type ' exit ' to EXIT Window"             )
                 print("                                           ")
                 httpd.serve_forever()
-                # return(et())         
- 
-main()
-
+mannual(start())
+rand(start())
